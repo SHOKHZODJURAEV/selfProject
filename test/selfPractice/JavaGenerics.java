ae71402dc0eb7f76d4e5d176105ca35fccbe1d9a
JURAEVSHOKHZOD <shawn.wang9393@gmail.com>
2022-01-09 18:14:53 -0500

    HelloWorld

---
+++ test/selfPractice/JavaGenerics.java
@@ -0,0 +1,50 @@
+package test.selfPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaGenerics {
+
+    public static void main(String[] args) {
+
+
+        List list = new ArrayList();
+        list.addAll(Arrays.asList(1));
+
+
+        int num = Integer.valueOf(list.get(0).toString());
+
+
+        System.out.println(list);
+
+
+        String s1 = new String("ABC"); //unmodifiable
+
+        // mutable
+        StringBuilder s2 = new StringBuilder("DEF");
+        StringBuffer s3 = new StringBuffer("GHI"); // synchronized
+
+
+    }
+
+
+    public synchronized void method1() {
+
+    }
+
+    public void method2() {
+
+
+        synchronized (this) {  // no static
+
+        }
+
+    }
+
+    public static void method3() {
+        synchronized (JavaGenerics.class) {  // static
+        }
+
+    }
+}
